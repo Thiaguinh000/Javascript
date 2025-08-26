d4129a0b77d42b5c7877a2e43f167a25cea59a2e
Thiago Silveira <thiagosilveira.ifc@gmail.com>
2025-08-26 09:34:30 -0300

    Versão calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,8 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+//Exemplo de uso:
+const resultadoSoma = funcoesMatematicas.somar(5, 3);
+console.log(`Resultado da soma: ${resultadoSoma}`);
+
+const resultadoSubtracao = funcoesMatematicas.subtrair(10, 4);
+console.log(`Resultado da subtração: ${resultadoSubtracao}`);
